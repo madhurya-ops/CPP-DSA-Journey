c332eb4f2f741b95690a6bff920b73a0c1d649d3
madhurya-ops <madhuryamishra@gmail.com>
2025-07-23 19:25:23 +0530

    Lec1

---
+++ Greedy_Algorithms/Lec1/fractional_knapsack.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+struct Item{
+int value;
+int weight;
+};
+
+bool comp(Item val1, Item val2) {
+    double r1 = (double)val1.value / val1.weight;
+    double r2 = (double)val2.value / val2.weight;
+    return r1 > r2;  // Higher value/weight comes first
+}
+
+double fractionalKnapsack(Item arr[], int n,int weight){
+    sort(arr, arr + n, comp);
+    double totalVal = 0.0;
+    for(int i = 0; i < n; i++){
+        if(arr[i].weight <= weight){
+            totalVal += arr[i].value;
+            weight -= arr[i].weight;
+        }
+        else{
+            totalVal += (double)(arr[i].value / arr[i].weight) * weight;
+            break;
+        }
+    }
+    return totalVal;
+}
+
+int main(){
+Item arr[] = { {60, 10}, {100, 20}, {120, 30} };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int W = 50;
+
+    cout << fractionalKnapsack(arr, n, W);
+}
