e32357ecff6c71ccf536222b523fa21416df2487
madhurya-ops <madhuryamishra@gmail.com>
2025-08-08 14:58:36 +0530

    Lec1

---
+++ Binary_Search_Trees/Lec1/search_BST.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+// Searching in a BST
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* searchBST(TreeNode* root, int val) {
+    while(root != NULL && root -> val != val){
+        root = val < root->val ? root -> left : root -> right;
+    }
+    return root;
+}
+
+int main(){
+// Tree
+struct TreeNode* root = new TreeNode(4);
+root -> left = new TreeNode(2);
+root -> right = new TreeNode(7);
+root -> left -> left = new TreeNode(1);
+root -> left -> right = new TreeNode(3);
+
+TreeNode* ans = searchBST(root, 7);
+
+cout << ans -> val;
+}
