d9acf6defe11fdc2128f8ea0e1877cbb50c7c4ef
madhurya-ops <madhuryamishra@gmail.com>
2025-09-01 14:36:46 +0530

    Lec3

---
+++ Graphs/Lec3/course_schedule_1.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+// return if the given sequence can be performed in the given order
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    // make the adjacency list out of the given sequence
+    vector<vector<int>> adj(numCourses);
+    for (auto &it : prerequisites) {
+        adj[it[1]].push_back(it[0]);
+    }
+
+    // store the indegrees of each node
+    vector<int> indegree(numCourses, 0);
+    for(int i = 0; i < numCourses; i++){
+        for(auto it : adj[i]){
+            indegree[it]++;
+        }
+    }
+
+    // push the node in the q with indegree = 0
+    queue<int> q;
+    for(int i = 0; i < numCourses; i++){
+        if(indegree[i] == 0) q.push(i);
+    }
+
+    // pop the node from q
+    // decrement its adj nodes' indegree
+    vector<int> topo;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+
+        // node is in your topo sort
+        // remove it from the indegree
+        for(auto it : adj[node]){
+            indegree[it]--;
+            if(indegree[it] == 0) q.push(it);
+        }
+    }
+        
+    if(topo.size() == numCourses) return true;
+    return false;
+}
+
+int main(){
+    int numCourses = 2;
+    vector<vector<int>> prerequisites = {{1, 0}};
+
+    bool result = canFinish(numCourses, prerequisites);
+    cout << (result ? "true" : "false") << endl;
+}
